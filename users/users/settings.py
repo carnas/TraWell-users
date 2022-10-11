1ae90d6e65d00c72d23550191b871cf36057b50c
carnas <julia13500@o2.pl>
2022-10-11 10:28:15 +0200

    Add models

--- users/users/settings.py
+++ users/users/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "users_management.apps.UsersManagementConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
--- users/users_management/models.py
+++ users/users_management/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+
+    class UserType(models.TextChoices):
+        INDIVIDUAL = 'Individual'
+        COMPANY = 'Company'
+
+    first_name = models.CharField(max_length=35)
+    last_name = models.CharField(max_length=70)
+    email = models.EmailField(max_length=100, unique=True)
+    date_of_birth = models.DateField()
+    user_type = models.TextField(max_length=20, choices=UserType.choices, default=UserType.INDIVIDUAL)
+    facebook = models.URLField(null=True, blank=True)
+    instagram = models.URLField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+
+class Vehicle(models.Model):
+    make = models.CharField(max_length=40)
+    model = models.CharField(max_length=40)
+    color = models.CharField(max_length=30)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
