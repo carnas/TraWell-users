5fd2f7ed55f8659575860b331a1ddf19d251ab78
carnas <julia13500@o2.pl>
2022-10-25 13:26:57 +0200

    Add get by id endpoint

--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserSerializer
+from users.models import User
 
-# Create your views here.
+
+class UserView(APIView):
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        users = User.objects.all()
+        return users
+
+    def get(self, request, *args, **kwargs):
+        id = request.query_params['id']
+        user = User.objects.get(user_id=id)
+        serializer = UserSerializer(user)
+
+        return Response(serializer.data)
+
+    @classmethod
+    def get_extra_actions(cls):
+        return []
--- users_service/settings.py
+++ users_service/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -76,13 +77,9 @@ WSGI_APPLICATION = "users_service.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "db",  # set in docker-compose.yml
-        "PORT": 5432,  # default postgres port
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
 
