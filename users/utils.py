172d133273a2b19e6e7dfceba9f7cdd4c1d58c73
carnas <julia13500@o2.pl>
2022-10-27 16:51:59 +0200

    Fix utils

--- users/utils.py
+++ users/utils.py
@@ -2,9 +2,6 @@ import jwt
 from email_validator import validate_email, EmailNotValidError
 
 
-ALGORITHM = "RS256"
-
-
 def get_user_data_from_jwt(encoded_jwt: str):
     data = decode_jwt(encoded_jwt)
     user_data = {'first_name': data['given_name'],
@@ -14,20 +11,20 @@ def get_user_data_from_jwt(encoded_jwt: str):
                  'user_type': rewrite_user_type(data['user_type']),
                  'facebook': data['facebook'],
                  'instagram': data['instagram'],
-                 'avatar': data['avatar']}
+                 'avatar': data['picture']}
 
     return user_data
 
 
 def decode_jwt(encoded_jwt: str):
-    data = jwt.decode(encoded_jwt)
+    data = jwt.decode(encoded_jwt, options={"verify_signature": False})
     return data
 
 
 def rewrite_user_type(user_type):
-    if user_type is 'Private Account':
+    if user_type == 'Private Account':
         return 'private'
-    elif user_type is 'Company Account':
+    elif user_type == 'Company Account':
         return 'company'
     else:
         return ''
