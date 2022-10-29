e5bf8d8244ec4400a1dfbb3fea262664a56b24a1
carnas <julia13500@o2.pl>
2022-10-29 21:55:53 +0200

    Add POST method functionality

--- vehicles/views.py
+++ vehicles/views.py
@@ -8,19 +8,29 @@ from django.http import JsonResponse
 from rest_framework import status
 
 
-class VehicleView(APIView):
-    serializer = VehicleWithoutUserSerializer
+@api_view(['GET', 'POST'])
+def user_vehicles(request, user_id):
+    try:
+        user = User.objects.get(user_id=user_id)
+    except User.DoesNotExist:
+        return JsonResponse(status=status.HTTP_404_NOT_FOUND, data=f'User with id={user_id} not found', safe=False)
 
-    @api_view(['GET'])
-    def user_vehicles(request):
-        if request.method == 'GET':
-            user_id = request.query_params['user_id']
-            try:
-                user = User.objects.get(user_id=user_id)
-                vehicles = Vehicle.objects.filter(user=user)
-                serializer = VehicleWithoutUserSerializer(vehicles, many=True)
+    if request.method == 'GET':
+        vehicles = Vehicle.objects.filter(user=user)
+        serializer = VehicleWithoutUserSerializer(vehicles, many=True)
+        return JsonResponse(status=status.HTTP_200_OK, data=serializer.data, safe=False)
 
-                return JsonResponse(status=status.HTTP_200_OK, data=serializer.data, safe=False)
-            except User.DoesNotExist:
-                return JsonResponse(status=status.HTTP_404_NOT_FOUND, data=f'User with id={user_id} not found', safe=False)
+    elif request.method == 'POST':
+        vehicle_data = {key: value for key, value in request.data.items() if key in ['model', 'make', 'color']}
+
+        try:
+            vehicle = Vehicle.objects.create(make=vehicle_data['make'], model=vehicle_data['model'],
+                                             color=vehicle_data['color'], user=user)
+            vehicle.save()
+            serializer = VehicleWithoutUserSerializer(vehicle)
+            return JsonResponse(status=status.HTTP_201_CREATED, data=serializer.data)
+        except KeyError:
+            return JsonResponse(status=status.HTTP_400_BAD_REQUEST, data="Wrong parameters", safe=False)
+        except ValidationError:
+            return JsonResponse(status=status.HTTP_400_BAD_REQUEST, data="Wrong parameters", safe=False)
 
