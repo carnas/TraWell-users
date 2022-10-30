8eb422788bd39ea6f3d0d2ef4667cbe1b4fed319
carnas <julia13500@o2.pl>
2022-10-30 20:21:18 +0100

    Adjust views to authorization functionality

--- users/views.py
+++ users/views.py
@@ -4,59 +4,69 @@ from rest_framework import status, permissions
 from rest_framework.decorators import api_view
 from .serializers import UserSerializer
 from users.models import User
-from users import utils
+from utils.authorization import is_authorized
 from django.core.exceptions import ValidationError
+
+from utils import users_utils
 
 
 @api_view(['GET'])
 def get_user(request, user_id):
-    try:
-        user = User.objects.get(user_id=user_id)
-    except User.DoesNotExist:
-        return JsonResponse(status=status.HTTP_404_NOT_FOUND, data=f'User with id={user_id} not found', safe=False)
+    authorized = is_authorized(request)
+    if authorized:
+        try:
+            user = User.objects.get(user_id=user_id)
+        except User.DoesNotExist:
+            return JsonResponse(status=status.HTTP_404_NOT_FOUND, data=f'User with id={user_id} not found', safe=False)
 
-    serializer = UserSerializer(user)
+        serializer = UserSerializer(user)
 
-    return JsonResponse(status=status.HTTP_200_OK, data=serializer.data)
+        return JsonResponse(status=status.HTTP_200_OK, data=serializer.data)
+    else:
+        return JsonResponse(status=status.HTTP_401_UNAUTHORIZED, data='Not authorized', safe=False)
 
 
 @api_view(['GET'])
-def check_user(self):
-    token = self.META.get('HTTP_AUTHORIZATION')
-    user_data = utils.get_user_data_from_token(token)
-    if 'error' not in user_data.keys():
-        email = user_data['email']
-        if utils.is_email_valid(email):
-            try:
-                user_with_email = User.objects.get(email=email)
-                # Get data that can be updated
-                user_data = {'first_name': user_data['first_name'],
-                             'last_name': user_data["last_name"],
-                             'date_of_birth': user_data['date_of_birth'],
-                             'facebook': user_data['facebook'],
-                             'instagram': user_data['instagram'],
-                             'avatar': user_data['avatar']
-                             }
-                serializer = UserSerializer(user_with_email, data=user_data, partial=True)
-                if serializer.is_valid():
-                    serializer.save()
-                    return JsonResponse(status=status.HTTP_200_OK, data=serializer.data)
-                else:
-                    return JsonResponse(status=status.HTTP_400_BAD_REQUEST, data="Wrong parameters", safe=False)
-            except User.DoesNotExist:
+def check_user(request):
+    authorized = is_authorized(request)
+    if authorized:
+        token = request.headers['Authorization']
+        user_data = users_utils.get_user_data_from_token(token)
+        if 'error' not in user_data.keys():
+            email = user_data['email']
+            if users_utils.is_email_valid(email):
                 try:
-                    new_user = User.objects.create(first_name=user_data['first_name'],
-                                                   last_name=user_data['last_name'],
-                                                   email=user_data['email'],
-                                                   date_of_birth=user_data['date_of_birth'],
-                                                   user_type=user_data['user_type'], facebook=user_data['facebook'],
-                                                   instagram=user_data['instagram'], avatar=user_data['avatar'])
-                    new_user.save()
-                    serializer = UserSerializer(new_user)
-                    return JsonResponse(status=status.HTTP_201_CREATED, data=serializer.data)
-                except ValidationError:
-                    return JsonResponse(status=status.HTTP_400_BAD_REQUEST, data="Wrong parameters", safe=False)
+                    user_with_email = User.objects.get(email=email)
+                    # Get data that can be updated
+                    user_data = {'first_name': user_data['first_name'],
+                                 'last_name': user_data["last_name"],
+                                 'date_of_birth': user_data['date_of_birth'],
+                                 'facebook': user_data['facebook'],
+                                 'instagram': user_data['instagram'],
+                                 'avatar': user_data['avatar']
+                                 }
+                    serializer = UserSerializer(user_with_email, data=user_data, partial=True)
+                    if serializer.is_valid():
+                        serializer.save()
+                        return JsonResponse(status=status.HTTP_200_OK, data=serializer.data)
+                    else:
+                        return JsonResponse(status=status.HTTP_400_BAD_REQUEST, data="Wrong parameters", safe=False)
+                except User.DoesNotExist:
+                    try:
+                        new_user = User.objects.create(first_name=user_data['first_name'],
+                                                       last_name=user_data['last_name'],
+                                                       email=user_data['email'],
+                                                       date_of_birth=user_data['date_of_birth'],
+                                                       user_type=user_data['user_type'], facebook=user_data['facebook'],
+                                                       instagram=user_data['instagram'], avatar=user_data['avatar'])
+                        new_user.save()
+                        serializer = UserSerializer(new_user)
+                        return JsonResponse(status=status.HTTP_201_CREATED, data=serializer.data)
+                    except ValidationError:
+                        return JsonResponse(status=status.HTTP_400_BAD_REQUEST, data="Wrong parameters", safe=False)
+            else:
+                return JsonResponse(status=status.HTTP_400_BAD_REQUEST, data=f"Email: {email} is not valid", safe=False)
         else:
-            return JsonResponse(status=status.HTTP_400_BAD_REQUEST, data=f"Email: {email} is not valid", safe=False)
+            return JsonResponse(status=status.HTTP_400_BAD_REQUEST, data=user_data['error'], safe=False)
     else:
-        return JsonResponse(status=status.HTTP_400_BAD_REQUEST, data=user_data['error'], safe=False)
+        return JsonResponse(status=status.HTTP_401_UNAUTHORIZED, data='Not authorized', safe=False)
--- vehicles/views.py
+++ vehicles/views.py
@@ -6,31 +6,36 @@ from vehicles.models import Vehicle
 from rest_framework.decorators import api_view
 from django.http import JsonResponse
 from rest_framework import status
+from utils.authorization import is_authorized
 
 
 @api_view(['GET', 'POST'])
 def user_vehicles(request, user_id):
-    try:
-        user = User.objects.get(user_id=user_id)
-    except User.DoesNotExist:
-        return JsonResponse(status=status.HTTP_404_NOT_FOUND, data=f'User with id={user_id} not found', safe=False)
-
-    if request.method == 'GET':
-        vehicles = Vehicle.objects.filter(user=user)
-        serializer = VehicleWithoutUserSerializer(vehicles, many=True)
-        return JsonResponse(status=status.HTTP_200_OK, data=serializer.data, safe=False)
-
-    elif request.method == 'POST':
-        vehicle_data = {key: value for key, value in request.data.items() if key in ['model', 'make', 'color']}
-
+    authorized = is_authorized(request)
+    if authorized:
         try:
-            vehicle = Vehicle.objects.create(make=vehicle_data['make'], model=vehicle_data['model'],
-                                             color=vehicle_data['color'], user=user)
-            vehicle.save()
-            serializer = VehicleWithoutUserSerializer(vehicle)
-            return JsonResponse(status=status.HTTP_201_CREATED, data=serializer.data)
-        except KeyError:
-            return JsonResponse(status=status.HTTP_400_BAD_REQUEST, data="Wrong parameters", safe=False)
-        except ValidationError:
-            return JsonResponse(status=status.HTTP_400_BAD_REQUEST, data="Wrong parameters", safe=False)
+            user = User.objects.get(user_id=user_id)
+        except User.DoesNotExist:
+            return JsonResponse(status=status.HTTP_404_NOT_FOUND, data=f'User with id={user_id} not found', safe=False)
+
+        if request.method == 'GET':
+            vehicles = Vehicle.objects.filter(user=user)
+            serializer = VehicleWithoutUserSerializer(vehicles, many=True)
+            return JsonResponse(status=status.HTTP_200_OK, data=serializer.data, safe=False)
+
+        elif request.method == 'POST':
+            vehicle_data = {key: value for key, value in request.data.items() if key in ['model', 'make', 'color']}
+
+            try:
+                vehicle = Vehicle.objects.create(make=vehicle_data['make'], model=vehicle_data['model'],
+                                                 color=vehicle_data['color'], user=user)
+                vehicle.save()
+                serializer = VehicleWithoutUserSerializer(vehicle)
+                return JsonResponse(status=status.HTTP_201_CREATED, data=serializer.data)
+            except KeyError:
+                return JsonResponse(status=status.HTTP_400_BAD_REQUEST, data="Wrong parameters", safe=False)
+            except ValidationError:
+                return JsonResponse(status=status.HTTP_400_BAD_REQUEST, data="Wrong parameters", safe=False)
+    else:
+        return JsonResponse(status=status.HTTP_401_UNAUTHORIZED, data='Not authorized', safe=False)
 
