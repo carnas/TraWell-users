029d98d2449b5b1219a86e6ec7585ddf3017a01d
carnas <julia13500@o2.pl>
2022-11-02 20:33:17 +0100

    Set url path

--- vehicles/urls.py
+++ vehicles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import user_vehicles
+from .views import user_vehicles, vehicle_details
 
 urlpatterns = [
     path('user_vehicles/<int:user_id>', user_vehicles),
+    path('user_vehicles/', vehicle_details),
 ]
